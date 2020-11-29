d437cd1b8ed4f12032adcc04d71c4607450bde1b
INfoUpgraders <infoupgraders@gmail.com>
2020-11-29 18:49:21 -0500

    🧰 GENERATE: Default main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::*;
+use rocket_contrib::templates::Template;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Welcome to the site!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
